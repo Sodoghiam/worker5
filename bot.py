3a90353773fcfe5611ca833ca21385db7a99a7d6
Sodoghiam <104337681+Sodoghiam@users.noreply.github.com>
2022-12-16 00:01:05 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,9 +58,7 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
-        self.LOGGER(__name__).info(f""" \n\n       
-
-                                          """)
+        self.LOGGER(__name__).info(f"kos")
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
